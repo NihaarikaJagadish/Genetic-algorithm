b4baeaafded788f03b76b12160ea80fa6eccf30f
NihaarikaJagadish <nihaarika1998@gmail.com>
2021-12-13 16:46:50 +0530

    After commenting

--- assignment2Final.py
+++ assignment2Final.py
@@ -15,30 +15,30 @@ class Individual(object):
         self.fitness = self.cal_fitness()
         
     @classmethod
-    def mutated_genes(self):
+    def mutated_genes(self): # This function is to randomly generate a rectangle
         x1 = random.randint(0, 21)
         y1 = random.randint(0, 21)
         size = random.randint(1,5)
-        shape = [(x1,y1),(x1 + size,y1+ size)]
+        shape = [(x1,y1),(x1 + size,y1+ size)] 
         return shape
 
     @classmethod
-    def create_gnome(self):
+    def create_gnome(self):#Creating rectangle for each state in a population
         return [self.mutated_genes() for _ in range(100)]
 
 
-    def mate(self, par2):
+    def mate(self, par2): # Responsible for mating. Both Cross over and mutation
         child_chromosome = []
         for gp1, gp2 in zip(self.chromosome, par2.chromosome):
             prob = random.random()
-            if prob < 0.45:
+            if prob < 0.45: #CrossOver
                 child_chromosome.append(gp1)
-            elif prob < 0.90:
+            elif prob < 0.90: #CrossOver
                 child_chromosome.append(gp2)
-            else:
+            else: #Mutation 
                 child_chromosome.append(self.mutated_genes())
         return Individual(child_chromosome)
-    def cal_fitness(self):
+    def cal_fitness(self): # Calculating the fitness of each individual.
         w, h = 32, 32
         img = Image.new("RGB", (w, h),"white")
         img1 = ImageDraw.Draw(img,'RGBA') 
@@ -47,21 +47,21 @@ class Individual(object):
         # img.show()
         pix = img.load()
         loss = 0
-        originalImage = Image.open('fin.jpg')
+        originalImage = Image.open('star.jpg')
         originalPixel = originalImage.load()
         for i in range(0,32):
             for j in range(0,32):
                 try:
-                    diffRed   = abs(originalPixel[i,j]   - pix[i,j][0])
+                    diffRed   = abs(originalPixel[i,j]   - pix[i,j][0]) #Comapring pixel by pixel the RGB values and finding its average
                     diffGreen  = abs(originalPixel[i,j]   - pix[i,j][1])
                     diffBlue  = abs(originalPixel[i,j]   - pix[i,j][2])
 
                     pctDiffRed   = diffRed / 255 
                     pctDiffGreen = diffGreen / 255
                     pctDiffBlue   = diffBlue  / 255
-                    loss = loss + (((pctDiffRed + pctDiffGreen + pctDiffBlue) / 3) * 100)
+                    loss = loss + (((pctDiffRed + pctDiffGreen + pctDiffBlue) / 3) * 100) #The sum of difference in each pixel is the fitness of each state
                 except:
-                    diffRed   = abs(originalPixel[i,j][0]   - pix[i,j][0])
+                    diffRed   = abs(originalPixel[i,j][0]   - pix[i,j][0]) #Some images have RBG as a tuple and some as int. Hence the Try/Except 
                     diffGreen  = abs(originalPixel[i,j][1]   - pix[i,j][1])
                     diffBlue  = abs(originalPixel[i,j][2]   - pix[i,j][2])
 
@@ -76,27 +76,27 @@ def main():
     generation = 1
     found = False
     population = []
-    for _ in range(POPULATION_SIZE):
+    for _ in range(POPULATION_SIZE): #Generates the whole population for each generation
         gnome = Individual.create_gnome()
         population.append(Individual(gnome))
     while not found:
-        population = sorted(population, key = lambda x:x.fitness)
+        population = sorted(population, key = lambda x:x.fitness) #Arranges the generation in the best fitness order
         print("\n\nLowestLoss: ",population[0].fitness)
-        if(generation % 100 == 0 or generation == 1):
+        if(generation % 100 == 0 or generation == 1): #Displays the first and every hundredth generation
             w, h = 32, 32
             img = Image.new("RGB", (w, h),"white")
             img1 = ImageDraw.Draw(img,'RGBA') 
-            for eachShape in population[0].chromosome:
+            for eachShape in population[0].chromosome: #To draw all the rectanges of the best state in the hundredth generation
                 img1.rectangle(eachShape, (105,105,105,125))
             img.show()
-        if population[0].fitness <= 15000:
+        if population[0].fitness <= 19000: #Stops when the difference in pixels in less than or equal to 15000
             found = True
             break
         new_generation = []
-        s = int((10*POPULATION_SIZE)/100)
+        s = int((10*POPULATION_SIZE)/100) #Chooses the top 10% of each population
         new_generation.extend(population[:s])
         s = int((90*POPULATION_SIZE)/100)
-        for _ in range(s):
+        for _ in range(s): #Generates the remaing 90% of the population by randomly mating the 10%
             parent1 = random.choice(population[:50])
             parent2 = random.choice(population[:50])
             count = 0
